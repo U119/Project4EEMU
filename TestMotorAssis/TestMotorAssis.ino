b3cd9f501baf2dcad460b5f56248ae4c72791266
Alongkorn S <98579450+U119@users.noreply.github.com>
2024-04-06 16:01:38 +0700

    Update TestMotorAssis.ino
    -แยก Task เพื่อทำงานพร้อมกัน
    -คิดเงื่ิอนไขเมื่อรถจอดในตำแหน่งที่ตรวจจับแม่เหล็กได้ให้นับเวลาช่วงนั้นไปด้วย

--- TestMotorAssis/TestMotorAssis.ino
+++ TestMotorAssis/TestMotorAssis.ino
@@ -39,48 +39,60 @@ TaskHandle_t Task2 = NULL;
 TaskHandle_t Task3 = NULL;
 int passValue = 0;
 
-void gyro_signals(void) {
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1A);
-  Wire.write(0x05);
-  Wire.endTransmission();
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1C);
-  Wire.write(0x10);
-  Wire.endTransmission();
-  Wire.beginTransmission(0x68);
-  Wire.write(0x3B);
-  Wire.endTransmission(); 
-  Wire.requestFrom(0x68,6);
-  int16_t AccXLSB = Wire.read() << 8 | Wire.read();
-  int16_t AccYLSB = Wire.read() << 8 | Wire.read();
-  int16_t AccZLSB = Wire.read() << 8 | Wire.read();
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1B); 
-  Wire.write(0x8);
-  Wire.endTransmission();                                                   
-  Wire.beginTransmission(0x68);
-  Wire.write(0x43);
-  Wire.endTransmission();
-  Wire.requestFrom(0x68,6);
-  int16_t GyroX=Wire.read()<<8 | Wire.read();
-  int16_t GyroY=Wire.read()<<8 | Wire.read();
-  int16_t GyroZ=Wire.read()<<8 | Wire.read();
-  RateRoll=(float)GyroX/65.5;
-  RatePitch=(float)GyroY/65.5;
-  RateYaw=(float)GyroZ/65.5;
-  AccX=(float)AccXLSB/4096 - 0.03;
-  AccY=(float)AccYLSB/4096;
-  AccZ=(float)AccZLSB/4096 - 0.17;
-  AngleRoll=atan(AccY/sqrt(AccX*AccX+AccZ*AccZ))*1/(3.142/180);
-  AnglePitch=-atan(AccX/sqrt(AccY*AccY+AccZ*AccZ))*1/(3.142/180);
+void Angle_Task(void *pvParam) {
+  while(1){
+    Wire.beginTransmission(0x68);
+    Wire.write(0x1A);
+    Wire.write(0x05);
+    Wire.endTransmission();
+    Wire.beginTransmission(0x68);
+    Wire.write(0x1C);
+    Wire.write(0x10);
+    Wire.endTransmission();
+    Wire.beginTransmission(0x68);
+    Wire.write(0x3B);
+    Wire.endTransmission(); 
+    Wire.requestFrom(0x68,6);
+    int16_t AccXLSB = Wire.read() << 8 | Wire.read();
+    int16_t AccYLSB = Wire.read() << 8 | Wire.read();
+    int16_t AccZLSB = Wire.read() << 8 | Wire.read();
+    Wire.beginTransmission(0x68);
+    Wire.write(0x1B); 
+    Wire.write(0x8);
+    Wire.endTransmission();                                                   
+    Wire.beginTransmission(0x68);
+    Wire.write(0x43);
+    Wire.endTransmission();
+    Wire.requestFrom(0x68,6);
+    int16_t GyroX=Wire.read()<<8 | Wire.read();
+    int16_t GyroY=Wire.read()<<8 | Wire.read();
+    int16_t GyroZ=Wire.read()<<8 | Wire.read();
+    RateRoll=(float)GyroX/65.5;
+    RatePitch=(float)GyroY/65.5;
+    RateYaw=(float)GyroZ/65.5;
+    AccX=(float)AccXLSB/4096 - 0.03;
+    AccY=(float)AccYLSB/4096;
+    AccZ=(float)AccZLSB/4096 - 0.17;
+    AngleRoll=atan(AccY/sqrt(AccX*AccX+AccZ*AccZ))*1/(3.142/180);
+    AnglePitch=-atan(AccX/sqrt(AccY*AccY+AccZ*AccZ))*1/(3.142/180);
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
+
+void IRAM_ATTR Proxit_Task() {
+  while(1) {
+    currentState = digitalRead(proxPin);
+    delay(100);
+  }
 }
 
 void setup() {
   Serial.begin(9600);
   delay(1000);
-  xTaskCreatePinnedToCore(Proxit_Task,"Task1",1000,NULL,1,&Task1,0);
-  xTaskCreatePinnedToCore(Velocity_Task,"Task1",1000,NULL,1,&Task2,0);
+  //xTaskCreatePinnedToCore(Proxit_Task,"Task1",1000,NULL,1,&Task1,0);
+  xTaskCreatePinnedToCore(Velocity_Task,"Task2",1000,NULL,1,&Task2,0);
+  xTaskCreatePinnedToCore(Angle_Task,"Task3",1000,NULL,1,&Task3,0);
+  attachInterrupt(digitalPinToInterrupt(proxPin), &Proxit_Task, CHANGE);
   // Setup motor
   pinMode(proxPin, INPUT);
   //attachInterrupt(digitalPinToInterrupt(proxPin), proximitInterrupt, CHANGE);
@@ -104,22 +116,18 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
-void Proxit_Task(void *pvParam) {
+void Velocity_Task(void *pvParam) {
   while(1) {
-    currentState = digitalRead(proxPin);
+    velocity = 0.05/time_ori;
+    F = (velocity * I * G) / (time_ori * p * R);
+    Ph = F * velocity;
+    Pm = 0.5 * Ph;
+    rpm = (Pm * 9.55) / Torque;
   }
 }
 
-void Velocity_Task(void *pvParam) {
-  velocity = 0.05/time_ori;
-  F = (velocity * I * G) / (time_ori * p * R);
-  Ph = F * velocity;
-  Pm = 0.5 * Ph;
-  rpm = (Pm * 9.55) / Torque;
-}
-
 void loop() {
-  gyro_signals();
+  
   if (!timing && lastState == LOW && currentState == HIGH) {
     t_start = millis() / 1000.0;
     timing = true;
@@ -158,9 +166,9 @@ void loop() {
   delay(10);
 
   //Ramp
-  if (AngleRoll > 8) {
+  if (AngleRoll > 8 && velocity != 0) {
     delay(1000);
-    if (AngleRoll > 8) {
+    if (AngleRoll > 8 && velocity != 0) {
       ledcWrite(0, motorSpeed);  // Set motor speed using LEDC
       digitalWrite(LPWM_Output, LOW); // Assuming LOW is forward direction
       while (i < 60000) { // Continue loop until i reaches 50
